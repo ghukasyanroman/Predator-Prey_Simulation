0b243abbf198637c7aea254efaf98a3837cef63c
Roman Ghukasyan <rghukasyan10@gmail.com>
2024-05-28 02:24:08 +0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,150 @@
+function generator(matLen, gr, grEat,st,pd,repd) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+    }
+    for (let i = 0; i < st; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+    }
+    for (let i = 0; i < pd; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 4;
+        }
+    }
+    for (let i = 0; i < repd; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 5;
+        }
+    }
+    return matrix;
+}
+
+let side = 20;
+
+let matrix = generator(40, 100, 50,10,13,10);
+
+
+
+
+var grassArr = [];
+var grassEaterArr = [];
+var PredatorArr = [];
+var RePredatorArr = [];
+function setup() {
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+
+
+    for (var y = 0; y < matrix.length; ++y) {
+        for (var x = 0; x < matrix[y].length; ++x) {
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y);
+                grassArr.push(gr);
+            }
+            else if (matrix[y][x] == 2) {
+                var great = new GrassEater(x, y);
+                grassEaterArr.push(great);
+            }
+            else if (matrix[y][x] == 4) {
+                var pd = new Predator(x, y);
+                PredatorArr.push(pd);
+            }
+            else if (matrix[y][x] == 5) {
+                var repd = new RePredator(x, y);
+                RePredatorArr.push(repd);
+            }
+        }
+    }
+
+
+}
+
+
+
+
+function draw() {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 3) {
+                fill("black");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 4) {
+                fill("brown");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 5) {
+                fill("pink");
+                rect(x * side, y * side, side, side);
+            }
+        }
+    }
+
+    for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].mul();
+        grassEaterArr[i].eat()
+    }
+    for (var i in PredatorArr) {
+        PredatorArr[i].mul();
+        PredatorArr[i].eat()
+    }
+    for (var i in RePredatorArr) {
+       if(PredatorArr.length<5){
+        RePredatorArr[i].move(4)
+       }
+       else{
+        RePredatorArr[i].move(0)
+       }
+    }
+}
+
+
+
+
+
+console.log(grassArr)
+
+
